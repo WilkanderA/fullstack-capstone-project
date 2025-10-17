0d0e809b6b966df800ad18cd717547c6850a988c
Wilkander <0xdemoneth@gmail.com>
2025-10-17 09:51:33 -0500

    Develop Back-end Integration for User Profile

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -100,4 +100,59 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.put('/update', async (req, res) => {
+    try {
+        // Task 2: Validate the input using `validationResult` and return appropriate message if there is an error.
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          logger.error('Validation errors in update request', errors.array());
+          return res.status(400).json({ errors: errors.array() });
+        }
+        
+        // Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
+        const email = req.headers.email;
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+        
+        // Task 4: Connect to MongoDB
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+        
+        // Task 5: find user credentials in database
+        const existingUser = await collection.findOne({ email });
+        
+        if (!existingUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+        
+        // Update user data with request body
+        if (req.body.firstName) existingUser.firstName = req.body.firstName;
+        if (req.body.lastName) existingUser.lastName = req.body.lastName;
+        existingUser.updatedAt = new Date();
+        
+        // Task 6: update user credentials in database
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+        
+        // Task 7: create JWT authentication using secret key from .env file
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        
+        logger.info('User updated successfully');
+        res.json({authtoken});
+    } catch (e) {
+         return res.status(500).send('Internal server error');
+    }
+});
+
 module.exports = router;
