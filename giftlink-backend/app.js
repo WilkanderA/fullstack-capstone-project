07478b6daef388128f17faf4e2db25c0433b7d1f
Wilkander <0xdemoneth@gmail.com>
2025-10-17 12:58:31 -0500

    fixed errors 3

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
 
 
 const app = express();
@@ -49,7 +48,7 @@ app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
